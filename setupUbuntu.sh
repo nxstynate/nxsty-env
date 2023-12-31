c36671367ef443b093d253b7318f8a5321b11038
nxstynate <nathanael.paul.lewis@gmail.com>
2023-12-31 14:37:21 -0600

    updated files.

--- setupUbuntu.sh
+++ setupUbuntu.sh
@@ -1,60 +1,48 @@
 #!/bin/bash
 
-echo "Installing FishShell..."
-sudo apt-add-repository ppa:fish-shell/release-3
-sudo apt-get update
-sudo apt-get install fish
+set -e
 
-echo "Installing Fisher..."
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-
-echo "Installing FZF..."
-sudo apt -y install fzf
-
-echo "Installing FD-Find..."
-sudo apt -y install fd-find
-
-echo "Installing bat..."
-brew install bat
-
-echo "Installing eza..."
+# sudo installation packages...
+echo "Running sudo update..."
 sudo apt update
-sudo apt -y install gpg
+
+# Install packages using apt
+echo "Installing packages with apt fish, tmux, git, fzf, fd-find, gpg, cmake, llvm..."
+sudo apt install -y fish tmux git fzf fd-find gpg cmake llvm
+
+# Add FishShell PPA and install
+echo "Adding FishShell PPA and installing..."
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update
+sudo apt install -y fish
+
+# Install eza
+echo "Installing eza..."
 sudo mkdir -p /etc/apt/keyrings
 wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
 echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
 sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
 sudo apt update
-sudo apt -y install eza
+sudo apt install -y eza
 
-echo "Installing Fisher: Tide and Z..."
-fish
-fisher install ilancosman/tide
-fisher install jethrokuan/z
-fihser install PatrickF1/fzf.fish
-exit
-
-echo "Installing Homebrew..."
+# Install Homebrew and packages
+echo "Installing Homebrew and packages..."
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >>~/.bashrc
+brew install bat lazygit neovim node gh
 
-echo "Installing Scoop"
-
-echo "Installing Neovim..."
-sudo apt-get update
-brew install neovim
-
-echo "Cloning LazyVim config..."
+# Git clone and setup configurations
+echo "Cloning and setting up configurations..."
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
-
-echo "Cloning personal Neovim configuration..."
 git clone https://github.com/nxstynate/config_files
 cp -rf config_files/neovim/nvim_devaslife_2024/* ~/.config/nvim/
+rm -rf config_files
 
-echo "Installing tmux..."
-sudo apt install -yl tmux
+# Install Fisher and packages
+echo "Installing Fisher and packages..."
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
+fish -c "fisher install jorgebucaran/fisher ilancosman/tide jethrokuan/z PatrickF1/fzf.fish"
 
-echo "LazyGit..."
-brew install lazygit
+echo "Installation complete..."
--- setupWindowsEnv.ps1
+++ setupWindowsEnv.ps1
@@ -1,69 +1,62 @@
-# Install chocolatey:
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# Set execution policy for the session
+Set-ExecutionPolicy Bypass -Scope Process -Force
 
-# Install scoop:
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+# Install package managers
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
-# Install packages:
-choco install powershell-core -y
-choco install git -y
-choco install gh -y
-choco install vscode -y
-choco install nodejs.install -y
-choco install fzf -y
-choco install neovim -y 
-choco install ripgrep -y
-choco install lazygit -y
-choco install bat -y
-choco install fd -y
-chcoo install mingw -y
-choco install cmake -y
-choco install llvm -y
-choco install python -y
-choco install pyenv-win -y
-choco install nerd-fonts-JetBrainsMono -y
-choco iinstall nerd-fonts-Hack -y
+# Install packages using Chocolatey
+$packages = 'powershell-core', 'git', 'gh', 'vscode', 'nodejs.install', 'fzf', 'neovim', 'ripgrep', 
+'lazygit', 'bat', 'fd', 'mingw', 'cmake', 'llvm', 'python', 'pyenv-win', 'nerd-fonts-JetBrainsMono', 
+'nerd-fonts-Hack'
+foreach ($package in $packages)
+{
+  choco install $package -y
+}
 
-winget install -e --id=Microsoft.WindowsTerminal
-winget install -e --id=Microsoft.PowerToys
-winget install -e --id=JanDeDobbeleer.OhMyPosh -s winget
-winget install -e --id=Google.Chrome
-winget install -e --id=Mozilla.Firefox
+# Install packages using winget
+$wingetPackages = 'Microsoft.WindowsTerminal', 'Microsoft.PowerToys', 'JanDeDobbeleer.OhMyPosh', 
+'Google.Chrome', 'Mozilla.Firefox'
+foreach ($app in $wingetPackages)
+{
+  winget install -e --id=$app
+}
 
-install-module terminal-icons -scope currentuser
-install-module PSReadLine -scope currentuser
-install-module posh-git -scope currentuser
-install-module oh-my-posh -scope currentuser
-install-module PSFzf -scope currentuser
-install-module -name z -scope currentuser
+# Install PowerShell modules
+$modules = 'terminal-icons', 'PSReadLine', 'posh-git', 'oh-my-posh', 'PSFzf', 'z'
+foreach ($module in $modules)
+{
+  Install-Module -Name $module -Scope CurrentUser -Force
+}
 
-# Install LazyVim Config:
+# Install LazyVim Config
 git clone https://github.com/LazyVim/starter $env:LOCALAPPDATA\nvim
 Remove-Item $env:LOCALAPPDATA\nvim\.git -Recurse -Force
 
-# Configuration settings for Powershell and Terminal:
-git clone https://github.com/nxstynate/config_files ~\Downloads\
-Copy-Item "~\Downloads\config_files\neovim\nvim_devaslife_2024\*" "$env:LOCALAPPDATA\nvim\." -Force -Recurse
-Copy-Item "~\Downloads\config_files\windows\PowerShell\Microsoft.PowerShell_profile.ps1" "~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Force -Recurse
-Copy-Item "~\Downloads\config_files\windows\PowerShell\powershell.config.json" "~\Documents\PowerShell\powershell.config.json" -Force -Recurse
-Copy-Item "~\Downloads\config_files\windows\PowerShell\takuya.omp.json" "~\Documents\PowerShell\takuya.omp.json" -Force -Recurse
-Copy-Item "~\Downloads\config_files\windows\PowerShell\takuya.omp.json.bak" "~\Documents\PowerShell\takuya.omp.json.bak" -Force -Recurse
-Copy-Item "~\Downloads\config_files\windows\Terminal\settings.json" "~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Force -Recurse
-Copy-Item "~\Downloads\config_files\windows\PowerToys\KeyboardManager\default.json" "~\AppData\Local\Microsoft\PowerToys\Keyboard Manager\default.json" -Force -Recurse
-Copy-Item "~\Downloads\config_files\windows\PowerToys\settings.json" "~\AppData\Local\Microsoft\PowerToys\settings.json" -Force -Recurse
+# Configuration settings for Powershell and Terminal
+$configSource = "~\Downloads\config_files"
+$nvimConfigSource = "$configSource\neovim\nvim_devaslife_2024\*"
+$powerShellConfigSource = "$configSource\windows\PowerShell\"
+$terminalConfigSource = "$configSource\windows\Terminal\settings.json"
+$powerToysConfigSource = "$configSource\windows\PowerToys\"
 
-Remote-Item "~\Downloads\config_files" -Recurse -Force
+Copy-Item $nvimConfigSource "$env:LOCALAPPDATA\nvim\." -Force -Recurse
+Copy-Item "$powerShellConfigSource\*" "~\Documents\PowerShell\" -Force -Recurse
+Copy-Item $terminalConfigSource "~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Force -Recurse
+Copy-Item "$powerToysConfigSource\*" "~\AppData\Local\Microsoft\PowerToys\" -Force -Recurse
 
+Remove-Item $configSource -Recurse -Force
+
+# Restart PowerToys to apply new settings
 Stop-Process -Name "PowerToys" -Force
-Start-Porcess -FilePath "C:\Program Files\PowerToys\PowerToys.exe"
+Start-Process -FilePath "C:\Program Files\PowerToys\PowerToys.exe"
 
-# Node package managers:
-npm install -g npm 
-npm install -global yarn
+# Node package managers
+npm install -g npm yarn
 Invoke-WebRequest https://get.pnpm.io/install.ps1 -useb | Invoke-Expression
 
-# Python package managers:
+# Python package manager
 pip install --user pipenv
 
 # WSL installation Ubuntu:
